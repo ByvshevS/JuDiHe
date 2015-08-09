e46d9fa28a2d785cc6f502c77fad69faed125d57
ByvshevS <byvshev.sergey@gmail.com>
2015-08-09 23:40:44 +0300

    db

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,31 @@
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 
 public class Main {
+	
+	public static void main(String[] args) {
+		try {
+			getConnection();
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private static Connection getConnection() throws URISyntaxException, SQLException {
+	    URI dbUri = new URI(System.getenv("DATABASE_URL"));
 
+	    String username = dbUri.getUserInfo().split(":")[0];
+	    String password = dbUri.getUserInfo().split(":")[1];
+	    String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
+
+	    return DriverManager.getConnection(dbUrl, username, password);
+	}
 }
