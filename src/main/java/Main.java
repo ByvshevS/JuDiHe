469539d8f88f8643ea21d76895fb5c30b2c3f107
ByvshevS <byvshev.sergey@gmail.com>
2015-08-11 15:51:24 +0300

    45

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -49,6 +49,10 @@ public class Main {
 		      }
 		   	});
 		
+		get("/test", (request, response) -> {
+            response.type("text/xml");
+            return "<?xml version=\"1.0\" encoding=\"UTF-8\"?><news> </news>";
+        });
 	
 		
 	}
